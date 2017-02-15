75dbc093b0a947632a57e50f8723afe315fce1ca
ramdhany <nirish777@gmail.com>
2017-02-15 12:06:30 +0000

    added iOS Framework build target

---
+++ PocketSocketFramework/PocketSocketFramework.h
@@ -0,0 +1,24 @@
+//
+//  PocketSocketFramework.h
+//  PocketSocketFramework
+//
+//  Created by Rajiv Ramdhany on 15/02/2017.
+//  Copyright © 2017 Zwopple Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PocketSocketFramework.
+FOUNDATION_EXPORT double PocketSocketFrameworkVersionNumber;
+
+//! Project version string for PocketSocketFramework.
+FOUNDATION_EXPORT const unsigned char PocketSocketFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PocketSocketFramework/PublicHeader.h>
+
+#import <PocketSocketFramework/PSWebSocket.h>
+#import <PocketSocketFramework/PSWebSocketServer.h>
+#import <PocketSocketFramework/PSWebSocketDriver.h>
+#import <PocketSocketFramework/PSWebSocketTypes.h>
+
+
